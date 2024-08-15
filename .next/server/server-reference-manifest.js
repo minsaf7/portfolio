1cc8fe1a357ddaf7dfadcfb1cdfc19f8dce356ec
minsaf7 <minsafmohamed7@gmail.com>
2024-08-15 18:33:39 +0530

    changes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ViZptQ3nQIq2leckzUJ7u+QGkEH+07M53YsFcnRZ2SU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2FLSqDiaQ7UX9mHQjWAYVZYcaDDkD3DGZVt3ZDHTMMc=\"\n}"
